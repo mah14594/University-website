d0857f08564f55855f89534d9149cca85a2d1ea5
alsalem.mahdi94@gmail.com <you@example.com>
2022-09-04 06:47:50 +0300

    add styles to wishlist items,add mark to wishListed items in the list

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -17,6 +17,7 @@ export class WishlistComponent implements OnInit {
     this.wishService.removeFromWishList(id);
   }
   toggleChecked(id: number) {
+    console.log(this.wishList);
     this.wishService.toggleCheckItem(id);
   }
 }
